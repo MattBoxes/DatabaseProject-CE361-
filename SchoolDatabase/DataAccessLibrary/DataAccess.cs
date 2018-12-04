2830cb2013690faa4705032b4d30403c7ae1609f
Justen Hitchcock <navyblue912@gmail.com>
2018-12-03 22:35:03 -0800

    First, I installed a new  NuGet package. I added a class library that contains functions for initialzing a database, adding data to the database, and geting data from the database. I also commented out the login page in the XAML code so I could test that the new code works.

---
+++ SchoolDatabase/DataAccessLibrary/DataAccess.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using System.Collections.Generic;
+
+namespace DataAccessLibrary
+{
+    public static class DataAccess
+    {
+        /// <summary>
+        /// Initializes the SQLite database.
+        /// </summary>
+        public static void InitializeDatabase()
+        {
+            using (SqliteConnection db =
+                new SqliteConnection("Filename=sqliteSample.db"))
+            {
+                db.Open();
+
+                String tableCommand = "CREATE TABLE IF NOT " +
+                    "EXISTS MyTable (Primary_Key INTEGER PRIMARY KEY, " +
+                    "Text_Entry NVARCHAR(2048) NULL)";
+
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+
+                createTable.ExecuteReader();
+            }
+        }
+
+        /// <summary>
+        /// Inserts data into the SQLite database.
+        /// </summary>
+        /// <param name="inputText"></param>
+        public static void AddData(string inputText)
+        {
+            using (SqliteConnection db =
+                new SqliteConnection("Filename=sqliteSample.db"))
+            {
+                db.Open();
+
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                // Use parameterized query to prevent SQL injection attacks
+                insertCommand.CommandText = "INSERT INTO MyTable VALUES (NULL, @Entry);";
+                insertCommand.Parameters.AddWithValue("@Entry", inputText);
+
+                insertCommand.ExecuteReader();
+
+                db.Close();
+            }
+
+        }
+
+        /// <summary>
+        /// Gets rows of data from a SQLite database.
+        /// </summary>
+        /// <returns></returns>
+        public static List<String> GetData()
+        {
+            List<String> entries = new List<string>();
+
+            using (SqliteConnection db =
+                new SqliteConnection("Filename=sqliteSample.db"))
+            {
+                db.Open();
+
+                SqliteCommand selectCommand = new SqliteCommand
+                    ("SELECT Text_Entry from MyTable", db);
+
+                SqliteDataReader query = selectCommand.ExecuteReader();
+
+                while (query.Read())
+                {
+                    entries.Add(query.GetString(0));
+                }
+
+                db.Close();
+            }
+
+            return entries;
+        }
+    }
+}
--- SchoolDatabase/SchoolDatabase/App.xaml.cs
+++ SchoolDatabase/SchoolDatabase/App.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using DataAccessLibrary;
 
 namespace SchoolDatabase
 {
@@ -30,6 +31,8 @@ namespace SchoolDatabase
         {
             this.InitializeComponent();
             this.Suspending += OnSuspending;
+
+            DataAccess.InitializeDatabase();
         }
 
         /// <summary>
