e20a214f2be601abe120e7f66318449ee41510cb
Xuong Truong <xtruong@sdsu.edu>
2018-12-04 19:50:51 -0800

    New class Student, Professor, Course. New StudentPage, ProfessorPage, AdminPage.

---
+++ SchoolDatabase/DataAccessLibrary/Course.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    class Course
+    {
+        public string Name { get; set; }
+
+        public short Id { get; set; }
+
+        public char Grade { get; set; }
+
+        public List<Student> ListOfStudents { get; set; }
+
+        public Professor Prof { get; set; }
+
+        public Course(string inName, short inID)
+        {
+            this.Name = inName;
+            this.Id = inID;
+        }
+
+    }
+}
