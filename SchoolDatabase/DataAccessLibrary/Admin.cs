13fc8c6400c4c4e9226f0863dbe9268559293c5a
carlos <carlos@DESKTOP-TR3T460>
2018-12-08 21:41:34 -0800

    constructors and some parameters

--- SchoolDatabase/DataAccessLibrary/Admin.cs
+++ SchoolDatabase/DataAccessLibrary/Admin.cs
@@ -8,5 +8,22 @@ namespace DataAccessLibrary
 {
     class Admin
     {
+        public string Name { get; set; }
+        public short Id { get; set; }
+
+        /// <summary>
+        /// Admin Constructor. The default name of the admin is "Admin". The default Id is "0"
+        /// </summary>
+        public Admin()
+        {
+            this.Name = "Admin";
+            this.Id = 0;
+        }
+        public Admin(string cName, short cId)
+        {
+            this.Name = cName;
+            this.Id = cId;
+        }
+
     }
 }
