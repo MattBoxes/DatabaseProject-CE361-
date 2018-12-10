c914c75cc690e67ee8d42b125e7e7f3d5de9a8cb
Xuong Truong <xtruong@sdsu.edu>
2018-12-09 18:16:09 -0800

    Added People parent class. Added database connection to adminpage, but still needs fixing. Also changed adminpage interface.

--- SchoolDatabase/DataAccessLibrary/Professor.cs
+++ SchoolDatabase/DataAccessLibrary/Professor.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace DataAccessLibrary
 {
-    class Professor
+    public class Professor : People
     {
+        public string Prof_Degree;
+        public List<Course> ListOfCourses;
+
+        public Professor(string firstname, string lastname, string id, string pw, string degree)
+            : base(firstname, lastname, id, pw)
+        {
+            this.Prof_Degree = degree;
+        }
+
+        public void DropStudentFromCourse (Student std, Course course)
+        {
+
+        }
+
+        public void ChangeGrade (Student std, char newGrade)
+        {
+
+        }
+
+        public void AddStudentToCourse (Student std, Course course)
+        {
+
+        }
     }
 }
--- SchoolDatabase/SchoolDatabase/AdminPage.xaml.cs
+++ SchoolDatabase/SchoolDatabase/AdminPage.xaml.cs
@@ -12,6 +12,10 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using SQLite.Net.Attributes;
+using DataAccessLibrary;
+
+
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,14 +26,36 @@ namespace SchoolDatabase
     /// </summary>
     public sealed partial class AdminPage : Page
     {
+        string path;
+        SQLite.Net.SQLiteConnection conn; 
+
         public AdminPage()
         {
             this.InitializeComponent();
+            path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "SchoolDB.db");
+            conn = new SQLite.Net.SQLiteConnection(new SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path);
+            conn.CreateTable<Course>();
         }
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(LoginPage));
+        }
+
+        private void AddCourse_Click(object sender, RoutedEventArgs e)
+        {
+            var add = conn.Insert(new Course(Course_Name.Text, Course_ID.Text));
+        }
+
+        private void ShowCoursesButton_Click(object sender, RoutedEventArgs e)
+        {
+            var query = conn.Table<Course>();
+            string result = String.Empty;
+            foreach (var item in query)
+            {
+                result = "{Course.Id} : {Course.Name}";
+                Listv.Items.Add(result);
+            }
         }
     }
 }
