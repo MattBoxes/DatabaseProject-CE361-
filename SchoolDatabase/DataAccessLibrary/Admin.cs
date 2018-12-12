d659a800c08f7f17a162b3bd9c7ea288f6ee30d9
carlos <carlos@DESKTOP-TR3T460>
2018-12-11 16:31:20 -0800

    admin class, changed ID for people from string to int

--- SchoolDatabase/DataAccessLibrary/Admin.cs
+++ SchoolDatabase/DataAccessLibrary/Admin.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
 
 namespace DataAccessLibrary
 {
-    public class Admin : People,IComparable
+    public class Admin : People,IComparable<Admin>
 
     {
         List<Course> ListOfCourses;
         List<People> ListOfUsers;
+
         /// <summary>
         /// Admin Constructor. The default name of the admin is "Admin". The default Id is "0"
         /// </summary>
-        public Admin(string firstname, string lastname, string id, string pw)
+        public Admin(string firstname, string lastname, int id, string pw)
             : base(firstname, lastname, id, pw)
         {
             this.FirstName = firstname;
@@ -22,68 +24,93 @@ namespace DataAccessLibrary
             this.Id = id;
             this.Password = pw;
         }
-        public Admin addCourse(string courseName)
+        public void addCourse(string courseName)
         {
 
         }
-        public Admin removeCourse(string courseName)
+        public void removeCourse(string courseName)
         {
 
         }
-        public Admin editCourseName(string courseName, string newCourseName)
+        public void editCourseName(string courseName, string newCourseName)
         {
 
         }
-        public Admin addUser(string firstname, string lastname, string id, string pw)
+        public void addUser(string firstname, string lastname, string id, string pw)
         {
 
         }
-        public Admin removeUser(string firstname, string lastname)
+        public void removeUser(string firstname, string lastname)
         {
 
         }
-        public Admin editUserName(string nfirstName, string nlastname, string firstname, string lastname)
+        public void editUserName(string nfirstName, string nlastname, string firstname, string lastname)
         {
 
         }
-        public Admin editStudentGrade(string firstname, string lastname, string coursename, string newGrade)
+        public void editStudentGrade(string firstname, string lastname, string coursename, string newGrade)
         {
 
         }
-        public Admin addStudentToCourse(string firstname, string lastname, string coursename)
+        public void addStudentToCourse(string firstname, string lastname, string coursename)
         {
 
         }
-        public Admin addProfToCourse(string firstname, string lastname, string coursename)
+        public void addProfToCourse(string firstname, string lastname, string coursename)
         {
 
         }
-        public Admin removeStudentFromCourse(string firstname, string lastname, string coursename)
+        public void removeStudentFromCourse(string firstname, string lastname, string coursename)
         {
 
         }
-        public Admin removeProfFromCourse(string firstname, string lastname, string coursename)
+        public void removeProfFromCourse(string firstname, string lastname, string coursename)
         {
 
         }
-        public Admin viewListOfCourses()
+        public void viewListOfCourses()
         {
-
+            throw new NotImplementedException();
         }
-        public Admin viewListOfUsers()
+        public void viewListOfUsers()
         {
-
+            throw new NotImplementedException();
         }
-        public Admin editStudentMajor(string firstname, string lastname, string newMajor)
+        public void editStudentMajor(string firstname, string lastname, string newMajor)
         {
-
+            throw new NotImplementedException();
         }
-        public Admin promoteUser(string firstname, string lastname)
+        public void promoteUser(string firstname, string lastname)
         {
-
+            throw new NotImplementedException();
         }
 
-        public int CompareTo(object obj)
+        public override bool Equals(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();   
+        }
+
+        private async void DisplayInvalidEntry()
+        {
+            ContentDialog InvalidEntry = new ContentDialog
+            {
+                Title = "Enter a current Admin's information",
+                CloseButtonText = "OK"
+            };
+            ContentDialogResult result = await InvalidEntry.ShowAsync();
+        }
+
+        public int CompareTo(Admin obj)
         {
             try
             {
@@ -93,12 +120,22 @@ namespace DataAccessLibrary
                 }
                 else
                 {
-
+                    if (LastName.CompareTo(obj.LastName) == 0)
+                    {
+                        if(FirstName.CompareTo(obj.FirstName) == 0)
+                        {
+                            return Id - obj.Id;
+                        }
+                        return FirstName.CompareTo(obj.FirstName);
+                    }
+                    return LastName.CompareTo(obj.LastName);
                 }
+
             }
             catch (NullReferenceException nEx)
             {
-                
+                DisplayInvalidEntry();
+                return -1;
             }           
         }
 
