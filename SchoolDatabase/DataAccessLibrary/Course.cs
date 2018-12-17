d22811a409317bcde01de7024aba9f2cdd54cf0b
carlos <carlos@DESKTOP-TR3T460>
2018-12-16 20:18:51 -0800

    Course CompareTo and ToString

--- SchoolDatabase/DataAccessLibrary/Course.cs
+++ SchoolDatabase/DataAccessLibrary/Course.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
 
 namespace DataAccessLibrary
 {
-    public class Course
+    public class Course : IComparable<Course>
     {
         public string Name { get; set; }
 
@@ -20,9 +21,44 @@ namespace DataAccessLibrary
             this.Id = inID;
         }
 
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        private async void DisplayInvalidCourseEntry()
+        {
+            ContentDialog InvalidEntry = new ContentDialog
+            {
+                Title = "Course information is invalid.",
+                CloseButtonText = "OK"
+            };
+            ContentDialogResult result = await InvalidEntry.ShowAsync();
+        }
+
+        public int CompareTo(Course obj)
+        {
+            if (obj == null)
+            {
+                DisplayInvalidCourseEntry();
+                return -1;
+            }
+            else
+            {
+                if (Name.CompareTo(obj.Name) == 0)
+                {
+                    return Id.CompareTo(obj.Id);
+                }
+                return Name.CompareTo(obj.Name);
+            }
+        }
+
         public override string ToString()
         {
-            return base.ToString();
+            if ((this.Name != null) && (this.Id != null))
+                return $"{this.Name}\n{this.Id}\n";
+            else
+                return $"Null character entered";
         }
     }
 }
